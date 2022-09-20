24a14e5fae3d02d7d83877e8dbdd59ff40e4ba51
Egil Ballestad <egil.ballestad@vigoiks.no>
2022-09-20 15:01:46 +0200

    added kafka ensuring of topic

--- src/main/java/no/fintlabs/integration/ActiveConfigurationIdRequestConsumerConfiguration.java
+++ src/main/java/no/fintlabs/integration/ActiveConfigurationIdRequestConsumerConfiguration.java
@@ -2,9 +2,11 @@ package no.fintlabs.integration;
 
 import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.integration.model.Integration;
+import no.fintlabs.kafka.common.topic.TopicCleanupPolicyParameters;
 import no.fintlabs.kafka.requestreply.ReplyProducerRecord;
 import no.fintlabs.kafka.requestreply.RequestConsumerFactoryService;
 import no.fintlabs.kafka.requestreply.topic.RequestTopicNameParameters;
+import no.fintlabs.kafka.requestreply.topic.RequestTopicService;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,8 +20,17 @@ public class ActiveConfigurationIdRequestConsumerConfiguration {
     @Bean
     public ConcurrentMessageListenerContainer<String, String> activeConfigurationIdRequestConsumer(
             RequestConsumerFactoryService requestConsumerFactoryService,
+            RequestTopicService requestTopicService,
             IntegrationRepository integrationRepository
     ) {
+        RequestTopicNameParameters requestTopicNameParameters = RequestTopicNameParameters
+                .builder()
+                .resource("active-configuration-id")
+                .parameterName("integration-id")
+                .build();
+        requestTopicService
+                .ensureTopic(requestTopicNameParameters, 0, TopicCleanupPolicyParameters.builder().build());
+
         return requestConsumerFactoryService.createFactory(
                 String.class,
                 String.class,
@@ -37,13 +48,7 @@ public class ActiveConfigurationIdRequestConsumerConfiguration {
                             .build();
                 },
                 new CommonLoggingErrorHandler()
-        ).createContainer(
-                RequestTopicNameParameters
-                        .builder()
-                        .resource("active-configuration-id")
-                        .parameterName("integration-id")
-                        .build()
-        );
+        ).createContainer(requestTopicNameParameters);
     }
 
 }
