a5732103331a606bbf1072afeab7890d9a608d56
Jan Ove Kongshaug <jan.ove.kongshaug@gmail.com>
2026-09-04 09:50:37 +0200

    fix: oppgrader Tomcat til 10.1.59 for å lukke kritiske sikkerhetshull (#69)
    Overstyrer tomcat.version via dependency-management-pluginen siden
    Spring Boot 3.5.16 fortsatt styrer Tomcat til sårbar 10.1.55.
    Lukker CVE-2026-68525, GHSA-9xv2-5v5q-p794 og GHSA-gcx9-497g-6cp6.
    
    FFS-2291

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,7 @@ repositories {
 extra["jackson-bom.version"] = "2.22.2"
 extra["log4j2.version"] = "2.26.1"
 extra["postgresql.version"] = "42.7.12"
+extra["tomcat.version"] = "10.1.59"
 
 dependencies {
     constraints {
